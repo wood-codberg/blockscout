0416945e6d0330536ac1b9a5b218edb575a68e15
wood-codberg <wood@codberg.com>
2022-12-16 14:31:47 +0900

    final stage

--- apps/block_scout_web/lib/block_scout_web/views/layout_view.ex
+++ apps/block_scout_web/lib/block_scout_web/views/layout_view.ex
@@ -48,11 +48,11 @@ defmodule BlockScoutWeb.LayoutView do
   end
 
   def subnetwork_title do
-    Keyword.get(application_config(), :subnetwork) || "Sokol"
+    Keyword.get(application_config(), :subnetwork) || "GMMT"
   end
 
   def network_title do
-    Keyword.get(application_config(), :network) || "POA"
+    Keyword.get(application_config(), :network) || ""
   end
 
   defp application_config do
